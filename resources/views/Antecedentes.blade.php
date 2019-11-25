16a719a91b44e66734a7764b1a9a0dff7c0892fe
OlafGonzalez <olaf.gonzalez1309@gmail.com>
2019-11-24 22:18:15 -0600

    Avance#5

---
+++ resources/views/Antecedentes.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Antecendentes</title>
+</head>
+<body>
+<div class="container">
+<h1>Antecendentes</h1>
+<label for="">Créditos fiscales</label>
+<input type="text"><br>
+<label for="">Colegio que realiza Avalúo:</label>
+<input type="text"><br>
+<label for="">Solicitante del Avalúo</label>
+<input type="text"><br>
+<div class="from-group">
+		<label>Telefono:</label>
+		<input type="number" placeholder="No. de telefono" name="tel" required>
+	</div><br>
+</div>
+</body>
+</html>
---
+++ resources/views/Captura.blade.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Inicio de captura</h1>
+
+    <label for="">Clave avalúo SHF</label>
+    <input type="text"><br>
+    
+    <label for="">Clave avalúo Banco (SEA)</label>
+    <input type="text"><br>
+
+    <label for="">Clave avalúo</label>
+    <input type="text"><br>
+
+    <label for="">Solicitante</label>
+    <input type="text"><br>
+
+    <label for="">RFC</label>
+    <input type="text"><br>
+
+    <label for="">Propietario</label>
+    <input type="text"><br>
+
+    <label for="">Domicilio</label>
+    <input type="text"><br>
+
+    <label for="">Fecha avalúo</label>
+    <input type="date" name="" id=""><br>
+
+    <label for="">Fecha ins</label>
+    <input type="date" name="" id=""><br>
+
+    <h1>Ubicación</h1>
+
+    <label for="">Calle</label>
+    <input type="text"><br>
+
+    <label for="">No.Exterior</label>
+    <input type="text"><br>
+
+    <label for="">No.interior</label>
+    <input type="text"><br>
+
+    <label for="">Conjunto(en su caso)</label>
+    <input type="text"><br>
+
+    <label for="">Colonia</label>
+    <input type="text"><br>
+
+    <label for="">Deleg. O Municipio</label>
+    <input type="text"><br>
+
+    <label for="">Entidad</label>
+    <input type="text"><br>
+
+    <label for="">Localidad</label>
+    <input type="text"><br>
+
+    <label for="">C.P</label>
+    <input type="text"><br>
+
+    <label for="">N° de cuenta predial</label>
+    <input type="text"><br>
+
+    <label for="">N° de cuenta del agua</label>
+    <input type="text"><br>
+
+    <label for="">Area o Cuenta de Valor:</label>
+    <input type="text"><br>
+
+    <input type="radio" name="inmueble_que_se_valua" id="">Casa
+    
+    
+
+
+    <h1>Objeto</h1>
+    <!-- Falta puntos-->
+    
+
+    <h1>Proposito</h1>
+    <input type="radio" name="Proposito" id="Proposito_id">Estimar el valor comercial del inmueble<br>
+    <input type="radio" name="Proposito" id="Proposito_id">Conocer el valor de las mejoras en la construcción<br>
+    <input type="radio" name="Proposito" id="Proposito_id">Conocer el valor del inmueble proyectado según presupuesto presentado<br>
+    <input type="radio" name="Proposito" id="Proposito_id">Conocer el valor comercial del inmueble en el estado en que se encuentra<br>
+    
+    <h1>Propósito</h1>
+
+    <input type="radio" name="" id="">Compra-venta<br>
+    <input type="radio" name="" id="">Escrituración<br>
+    <input type="radio" name="" id="">Conocimiento del valor comercial<br>
+    <input type="radio" name="" id="">Garantía de crédito, para prestamo bancario<br>
+    <input type="radio" name="" id="">Uso interno de la Institución Solicitante. ( NO TIENE VALIDEZ)<br>
+    <input type="radio" name="" id="">Hacer efectivo crédito fiscal<br>
+
+    <h1>Régimen</h1>
+    <input type="radio" name="" id="">Privada individual<br>
+    <input type="radio" name="" id="">Privada colectiva en condominio<br>
+    <input type="radio" name="" id="">Copropiedad<br>
+    <input type="radio" name="" id="">Ejidal<br>
+    <input type="radio" name="" id="">Federal<br>
+
+    <h2>DECLARACIONES: SE REALIZARON LAS SIGUIENTES </h2>
+
+    <label for="">¿La identificación física del inmueble coincide con lo señalado en la documentación?</label><br>
+    <input type="radio" name="idp1" id="">Si<br>
+    <input type="radio" name="idp1" id="">No<br>
+
+    <label for="">¿las superficies físicas observadas coinciden con la documentación ?</label><br>
+    <input type="radio" name="idp2" id="">Si<br>
+    <input type="radio" name="idp2" id="">No<br>
+
+    <label for="">¿Se verifico el estado de la construcción y conservación del inmueble?</label><br>
+    <input type="radio" name="idp3" id="">Si<br>
+    <input type="radio" name="idp3" id="">No<br>
+
+    <label for="">¿Se verifico el estado actual del inmueble y su uso ?</label><br>
+    <input type="radio" name="idp4" id="">Si<br>
+    <input type="radio" name="idp4" id="">No<br>
+
+    <label for="">¿El inmueble es considerado monumento histórico por el INAH ?</label><br>
+    <input type="radio" name="idp5" id="">Si<br>
+    <input type="radio" name="idp5" id="">No<br>
+
+    <label for="">¿El inmueble es considerado monumento histórico por el INBA ?</label><br>
+    <input type="radio" name="idp6" id="">Si<br>
+    <input type="radio" name="idp6" id="">No<br>
+
+    <h3>ADVERTENCIAS:</h3>
+    <h4>No se dispuso de documentacion relevante</h4>
+    <h4>No se encuentran ofertas de mercado en la zona para considerar el enfoque comparativo de mercado</h4>
+    <h4>Existe duda sobre el uso del inmueble o de laguna sección del mismo</h4>
+    <h4>Existen obras públicas  o privadas que afectan los servicios en la colonia</h4>
+
+    <h3>Señalar aquí otra Advertencia en su caso:</h3>
+    <div class="md-form">
+            <textarea id="form6" class="md-textarea form-control" rows="3"></textarea>
+            <label for="form7">Material textarea</label>
+    </div>
+    <h3>Señalar aquí otra Límitante del avalúo:</h3>
+    <div class="md-form">
+            <textarea id="form7" class="md-textarea form-control" rows="3"></textarea>
+            <label for="form7">Material textarea</label>
+    </div>
+
+    <h1>Clasificación de la Zona</h1>
+
+    <input type="radio" name="clas_zona" id="clas_zona_id">Habitacional de 1er. orden.<br>
+    <input type="radio" name="clas_zona" id="clas_zona_id">Habitacional de 2do. orden.<br>
+    <input type="radio" name="clas_zona" id="clas_zona_id">Habitacional de 3er. orden.<br>
+    <input type="radio" name="clas_zona" id="clas_zona_id">Habitacional de tipo exclusivo<br>
+    <input type="radio" name="clas_zona" id="clas_zona_id">Habitacional de interes social.<br>
+    <input type="radio" name="clas_zona" id="clas_zona_id">Mixta, habitacional y comercial de 2do. orden.<br>
+    <input type="radio" name="clas_zona" id="clas_zona_id">Mixta, habitacional y comercial de 3er. orden.<br>
+    <input type="radio" name="clas_zona" id="clas_zona_id">Mixta, habitacional, comercial y de servicios de 2do. orden.<br>
+    <input type="radio" name="clas_zona" id="clas_zona_id">Mixta, habitacional, comercial y de servicios de 1er. orden.<br>
+    <input type="radio" name="clas_zona" id="clas_zona_id">Habitacional con industria mezclada de 2do. Orden.<br>
+    <input type="radio" name="clas_zona" id="clas_zona_id">Industrial de tipo mediano.<br>
+    <input type="radio" name="clas_zona" id="clas_zona_id">en transición<br>
+
+    <h1>Proximidad urbana:</h1>
+
+    <input type="radio" name="Proximidad" id="Proximidad_id">Centrica<br>
+    <input type="radio" name="Proximidad" id="Proximidad_id">Intermedia<br>
+    <input type="radio" name="Proximidad" id="Proximidad_id">Periferica<br>
+
+
+</body>
+</html>
